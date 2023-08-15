4b21b56c2d58d60535afe63f5b9887efea73d7e1
Bruno Ribeiro <bruno.oliveira@bandtec.com.br>
2023-08-14 23:52:58 -0300

    REFACT(Formulario): Refatorando componentes

--- alura-tracker/src/components/Formulario.vue
+++ alura-tracker/src/components/Formulario.vue
@@ -2,24 +2,10 @@
     <div class="box">
         <div class="columns">
             <div class="column is-8" role="form" aria-label="Formulário para criação de uma nova tarefa">
-                <input type="text" class="input" placeholder="Qual tarefa você deseja iniciar?">
+                <input type="text" class="input" placeholder="Qual tarefa você deseja iniciar?" v-model="descricao">
             </div>
             <div class="column">
-                <div class="is-flex is-align-items-center is-justify-content-space-between">
-                    <Cronometro :tempoEmSegundos="tempoEmSegundos"/>
-                    <button class="button" @click="iniciar">
-                        <span class="icon">
-                            <i class="fas fa-play"></i>
-                        </span>
-                        <span>play</span>
-                    </button>
-                    <button class="button" @click="finalizar">
-                        <span class="icon">
-                            <i class="fas fa-stop"></i>
-                        </span>
-                        <span>stop</span>
-                    </button>
-                </div>
+                <Temporizador @aoTemporizadorFinalizado="finalizarTarefa"/>
             </div>
         </div>
     </div>
@@ -27,25 +13,20 @@
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import Cronometro from './Cronometro.vue';
+import Temporizador from './Temporizador.vue';
 
 export default defineComponent({
     name: "Formulario",
-    components: { Cronometro },
-    data() {
+    components: { Temporizador },
+    data (){
         return {
-            tempoEmSegundos: 0,
-            cronometro: 0
-        };
+            descricao: ''
+        }
     },
     methods: {
-        iniciar() {
-            this.cronometro = setInterval(() => {
-                this.tempoEmSegundos += 1;
-            }, 1000);
-        },
-        finalizar() {
-            clearInterval(this.cronometro);
+        finalizarTarefa(tempoDecorrido: number): void {
+            console.log(tempoDecorrido)
+            this.descricao = ''
         }
     }
 })
