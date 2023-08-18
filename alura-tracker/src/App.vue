4693216753176e717895a93375d0a9c66f60def5
Bruno Ribeiro <bruno.oliveira@bandtec.com.br>
2023-08-17 23:54:26 -0300

    FEAT(App): Criando dark-theme

--- alura-tracker/src/App.vue
+++ alura-tracker/src/App.vue
@@ -1,9 +1,9 @@
 <template>
-  <main class="columns is-gapless is-multiline">
+  <main class="columns is-gapless is-multiline" :class="{ 'modo-escuro': modoEscuroAtivo }">
     <div class="column is-one-quarter">
-      <BarraLateral/>
+      <BarraLateral @aoTemaAlterado="trocarTema"/>
     </div>
-    <div class="column is-three-quarter">
+    <div class="column is-three-quarter conteudo">
       <Formulario @aoSalvarTarefa="salvarTarefa" />
       <div class="lista">
         <Tarefa v-for="(tarefa,index) in tarefas" :key="index" :tarefa="tarefa" />
@@ -29,7 +29,8 @@ export default defineComponent({
     components: { BarraLateral, Formulario, Tarefa, Box },
     data() {
       return {
-        tarefas: [] as ITarefa[]
+        tarefas: [] as ITarefa[],
+        modoEscuroAtivo: false
       }
     },
     computed: {
@@ -40,7 +41,10 @@ export default defineComponent({
     methods: {
       salvarTarefa(tarefa: ITarefa){
         this.tarefas.push(tarefa)
-      } 
+      },
+      trocarTema(modoEscuroAtivo: boolean){
+        this.modoEscuroAtivo = modoEscuroAtivo
+      }
     }
 })
 </script>
@@ -48,5 +52,16 @@ export default defineComponent({
 <style>
   .lista {
     padding: 1.25rem;
+  }
+  main{
+    --bg-primario: #fff;
+    --texto-primario: #000;
+  }
+  main.modo-escuro {
+    --bg-primario: #2b2d42;
+    --texto-primario: #ddd;
+  }
+  .conteudo{
+    background-color: var(--bg-primario);
   }
 </style>
