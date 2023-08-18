29aa8b4c324308d68d0035addbdea9b7757e29d5
Bruno Ribeiro <bruno.oliveira@bandtec.com.br>
2023-08-17 23:55:41 -0300

    FEAT(Cronometro): Tornando estilo dinamico

--- alura-tracker/src/components/Cronometro.vue
+++ alura-tracker/src/components/Cronometro.vue
@@ -1,6 +1,6 @@
 <template>
     <section>
-        <strong>
+        <strong class="display">
             {{ tempoDecorrido }}
         </strong>
     </section>
@@ -27,4 +27,7 @@ export default defineComponent({
 </script>
 
 <style>
+.display{
+    color: var(--texto-primario)
+}
 </style>
