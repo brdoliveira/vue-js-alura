5c0b9434eb017722dceaa1cdd1999c5039714d60
Bruno Ribeiro <bruno.oliveira@bandtec.com.br>
2023-08-27 23:58:28 -0300

    FEAT(notificador):  Criando hook Notificador

---
+++ alura-tracker/src/hooks/notificador.ts
@@ -0,0 +1,17 @@
+import { TipoNotificacao } from "@/interfaces/INotificacao"
+import { NOTIFICAR } from "@/store/tipo-mutacoes"
+import { store } from "@/store"
+
+type Notificador = {
+    notificar: (tipo: TipoNotificacao, titulo: string, texto: string) => void
+}
+
+export default (): Notificador => {
+    const notificar = (tipo, titulo, texto): void => {
+        store.commit(NOTIFICAR, {
+            titulo: titulo,
+            texto: texto,
+            tipo: tipo
+        })
+    }
+}
