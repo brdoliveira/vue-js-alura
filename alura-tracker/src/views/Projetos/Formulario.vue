078c9a3a8b11362a917a9dc546a5a5aef0b0c797
Bruno Ribeiro <bruno.oliveira@bandtec.com.br>
2023-08-31 23:59:13 -0300

    REFACT(Formulario): Conectando formulario com api

--- alura-tracker/src/views/Projetos/Formulario.vue
+++ alura-tracker/src/views/Projetos/Formulario.vue
@@ -21,9 +21,9 @@
 <script lang="ts">
 import { defineComponent } from 'vue';
 import { useStore } from '@/store';
-import { ALTERA_PROJETO, ADICIONA_PROJETO } from '@/store/tipo-mutacoes'
 import { TipoNotificacao } from '@/interfaces/INotificacao';
 import { notificacaoMixin } from '@/mixins/notificar'
+import { ALTERAR_PROJETO, CADASTRAR_PROJETO } from '@/store/tipo-acoes';
 
 export default defineComponent({
     name: 'Formulario',
@@ -47,13 +47,19 @@ export default defineComponent({
     methods: {
         salvar(){
             if (this.id) {
-                this.store.commit(ALTERA_PROJETO, {
+                this.store.dispatch(ALTERAR_PROJETO, {
                     id: this.id,
                     nome: this.nomeDoProjeto
+                }).then(() => {
+                    this.lidarComSucesso()
                 })
             } else {
-                this.store.commit(ADICIONA_PROJETO, this.nomeDoProjeto)
+                this.store.dispatch(CADASTRAR_PROJETO, this.nomeDoProjeto).then(() => {
+                    this.lidarComSucesso()
+                })
             }
+        },
+        lidarComSucesso(){
             this.nomeDoProjeto = ''
             this.notificar(
                 TipoNotificacao.SUCESSO,
